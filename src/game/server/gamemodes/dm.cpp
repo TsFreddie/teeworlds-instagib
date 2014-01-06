7ff6e4d42fe3e69d8608a8d4e4d514021a992324
Freddie Wang <freddiew@qq.com>
2014-01-07 00:55:49 +0800

    First Commit of Instagib
    Have Fun!

---
+++ src/game/server/gamemodes/dm.cpp
@@ -0,0 +1,15 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+#include "dm.h"
+
+
+CGameControllerDM::CGameControllerDM(class CGameContext *pGameServer)
+: IGameController(pGameServer)
+{
+	m_pGameType = "iDM";
+}
+
+void CGameControllerDM::Tick()
+{
+	IGameController::Tick();
+}
